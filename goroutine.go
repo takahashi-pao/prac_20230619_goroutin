fdfd9ef10fa58a0f3eaf46851199f2b9fc728082
takahashi-pao <a.mapi.baseball@gmail.com>
2023-06-20 21:04:22 +0900

    go routine by GET request

--- goroutine.go
+++ goroutine.go
@@ -2,19 +2,45 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
+	go makeReqest("https://qiita.com/ohbashunsuke/items/e7c673db606a6dced8a6")
+
 	go count("goroutine")
 	count("main")
 
-	time.Sleep(time.Second)
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(input)
 }
 
 func count(label string) {
 	for i := 1; i <= 5; i++ {
+		for n := 1; n <= 10000000; n++ {
+
+		}
 		fmt.Println(label, i)
-		time.Sleep(time.Microsecond * 500)
+		// time.Sleep(time.Microsecond * 500)
 	}
+}
+
+func makeReqest(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	fmt.Println("Response:", string(body))
 }
